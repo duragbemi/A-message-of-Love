f5625e5e4d7d2e08e7d512533ab1b9f2afb0a3f2
duragbemi <bamideleaduragbemi00@gmail.com>
2026-02-02 15:50:36 +0100

    Update Deploy

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -2,6 +2,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://qsgitrteqwmqqhocuxsc.supabase.co'
-const supabaseKey = 'sb_publishable_6kIs0dIKHxXVowXW-Wn4Vg_38VTaX8T'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFzZ2l0cnRlcXdtcXFob2N1eHNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAwMTc5MzEsImV4cCI6MjA4NTU5MzkzMX0.0UQfVk0c_MpHwoqW7RsAKZStrBMyxpNpcKAwE9CB37I'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
